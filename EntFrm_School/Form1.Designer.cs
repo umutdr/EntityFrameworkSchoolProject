8124735663f6469d18d7908b961f56b38ffad031
Umut <umutdr98@gmail.com>
2019-12-01 16:43:42 +0300

    Add project files.

---
+++ EntFrm_School/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace EntFrm_School
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUndo = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnPrevEntry = new System.Windows.Forms.Button();
+            this.btnFirstEntry = new System.Windows.Forms.Button();
+            this.btnLastEntry = new System.Windows.Forms.Button();
+            this.bnNextEntry = new System.Windows.Forms.Button();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSearch);
+            this.groupBox1.Controls.Add(this.txtSearch);
+            this.groupBox1.Location = new System.Drawing.Point(461, 354);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(277, 73);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(177, 33);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(90, 30);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(14, 34);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(150, 29);
+            this.txtSearch.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1200, 342);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnDel);
+            this.groupBox3.Controls.Add(this.btnAdd);
+            this.groupBox3.Controls.Add(this.btnUndo);
+            this.groupBox3.Controls.Add(this.btnUpdate);
+            this.groupBox3.Location = new System.Drawing.Point(763, 352);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(424, 75);
+            this.groupBox3.TabIndex = 14;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Actions";
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(114, 34);
+            this.btnDel.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(90, 30);
+            this.btnDel.TabIndex = 8;
+            this.btnDel.Text = "Del";
+            this.btnDel.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(9, 34);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(90, 30);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnUndo
+            // 
+            this.btnUndo.Location = new System.Drawing.Point(324, 34);
+            this.btnUndo.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(90, 30);
+            this.btnUndo.TabIndex = 4;
+            this.btnUndo.Text = "Undo";
+            this.btnUndo.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(219, 34);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(90, 30);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnPrevEntry);
+            this.groupBox2.Controls.Add(this.btnFirstEntry);
+            this.groupBox2.Controls.Add(this.btnLastEntry);
+            this.groupBox2.Controls.Add(this.bnNextEntry);
+            this.groupBox2.Location = new System.Drawing.Point(12, 353);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(424, 75);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Navigation";
+            // 
+            // btnPrevEntry
+            // 
+            this.btnPrevEntry.Location = new System.Drawing.Point(114, 34);
+            this.btnPrevEntry.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.btnPrevEntry.Name = "btnPrevEntry";
+            this.btnPrevEntry.Size = new System.Drawing.Size(90, 30);
+            this.btnPrevEntry.TabIndex = 8;
+            this.btnPrevEntry.Text = "<";
+            this.btnPrevEntry.UseVisualStyleBackColor = true;
+            // 
+            // btnFirstEntry
+            // 
+            this.btnFirstEntry.Location = new System.Drawing.Point(9, 34);
+            this.btnFirstEntry.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.btnFirstEntry.Name = "btnFirstEntry";
+            this.btnFirstEntry.Size = new System.Drawing.Size(90, 30);
+            this.btnFirstEntry.TabIndex = 9;
+            this.btnFirstEntry.Text = "<<<";
+            this.btnFirstEntry.UseVisualStyleBackColor = true;
+            // 
+            // btnLastEntry
+            // 
+            this.btnLastEntry.Location = new System.Drawing.Point(324, 34);
+            this.btnLastEntry.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.btnLastEntry.Name = "btnLastEntry";
+            this.btnLastEntry.Size = new System.Drawing.Size(90, 30);
+            this.btnLastEntry.TabIndex = 4;
+            this.btnLastEntry.Text = ">>>";
+            this.btnLastEntry.UseVisualStyleBackColor = true;
+            // 
+            // bnNextEntry
+            // 
+            this.bnNextEntry.Location = new System.Drawing.Point(219, 34);
+            this.bnNextEntry.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.bnNextEntry.Name = "bnNextEntry";
+            this.bnNextEntry.Size = new System.Drawing.Size(90, 30);
+            this.bnNextEntry.TabIndex = 6;
+            this.bnNextEntry.Text = ">";
+            this.bnNextEntry.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 445);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Font = new System.Drawing.Font("Segoe UI Semilight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUndo;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnPrevEntry;
+        private System.Windows.Forms.Button btnFirstEntry;
+        private System.Windows.Forms.Button btnLastEntry;
+        private System.Windows.Forms.Button bnNextEntry;
+        private System.Windows.Forms.BindingSource bindingSource1;
+    }
+}
+
